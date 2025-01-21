2852e744a8759fae053b6069beacefcc979361de
Andres Vergauwen <andres.vergauwen@hotmail.com>
2025-01-21 10:50:58 +0100

    Fix ts error

--- src/index.ts
+++ src/index.ts
@@ -16,7 +16,7 @@ import {
   loadJobTemplates,
   setJobTemplateCreeationDefaults,
 } from "./job/job-template.js";
-import { getJobs, loadJobs, setJobCreationDefaults } from "./job/job.js";
+import { getJobs, setJobCreationDefaults } from "./job/job.js";
 import {
   DataMonitoringFunction,
   JobStatus,
